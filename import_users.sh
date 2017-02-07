ed42129c2b3acf0113a6ba5244c98ce2b345220e
Gonzalo Martín Cruz <gonzalo.martin@momit.com>
2017-02-07 10:35:59 +0100

    Restore sudo privileges for all users

--- import_users.sh
+++ import_users.sh
@@ -8,9 +8,9 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   SaveUserName=${SaveUserName//"@"/".at."}
   if ! grep "^$SaveUserName:" /etc/passwd > /dev/null; then
     # sudo will read each file in /etc/sudoers.d, skipping file names that end in ‘~’ or contain a ‘.’ character to avoid causing problems with package manager or editor temporary/backup files.
-    /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName" 
+    /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName"
     # Uncomment the following lines if you need to give all users sudo privileges
-    # SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
-    # echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$SaveUserFileName"
+    SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
+    echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$SaveUserFileName"
   fi
 done
